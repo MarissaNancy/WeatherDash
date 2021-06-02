8fb791fafe3d729bccfd1ec2395ba97514acbe6c
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-02 16:31:06 -0700

    added function

--- script.js
+++ script.js
@@ -2,17 +2,26 @@
 //think what the user has to do in order to start with the app
 
 
-var ApIkey = "592fe29192cc287a00ec4f4aa68923ec"
+var APIkey = "592fe29192cc287a00ec4f4aa68923ec"
 var searchBttn = document.querySelector('#inputSearch');
 var userInput = document.querySelector('#userinput');
+var cityTitle = document.querySelector('#cardTitle');
+var weatherInfo = document.querySelector('#cardBody');
+var day = document.querySelector('#day')
 
 function searchCity() {
-  var city = userInput.nodeValue;
-  var URL = "http://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" + APIkey
+  var city = userInput.value;
+  var URL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=imperial&appid=" + APIkey
 
+  console.log(city)
   fetch(URL)
   .then(function(res){
-    console.log(res)
+    return res.json();
+  }).then(function(results){
+    console.log(results);
+    cityTitle.textContent = results.city.name
+    weatherInfo.textContent = 'temp: C ' + results.list[0].main.temp
+   
   })
 }
 
@@ -21,9 +30,9 @@ searchBttn.addEventListener('click', searchCity)
 //make variables for card containers to append one 
 
 //function to print results
-function printRes(){
-  //needs parameterd and can det link.setattribute or inner.html 
-}
+// function printRes(){
+//   //needs parameterd and can det link.setattribute or inner.html 
+// }
 
 //this function to pass on city name from user
 // function getCityName(event) {
